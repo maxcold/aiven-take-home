3933de2643d1b60dd94cba4586e7249e795ceaf5
Maxim Kholod <maxim.kholod@klarna.com>
2023-03-11 12:48:36 +0100

    add dumb cache

--- server/modules/clouds/routes.ts
+++ server/modules/clouds/routes.ts
@@ -2,14 +2,32 @@ import express, { NextFunction, Request, Response, Router } from 'express';
 import fetch from 'node-fetch';
 import * as config from '../../config.js';
 
+interface Cloud {
+    cloud_name: string;
+    cloud_description: string;
+    geo_longitude: number;
+    geo_latitude: number;
+    geo_region: string;
+    provider: string;
+    provider_description: string;
+}
+interface Clouds {
+    clouds: Array<Cloud>;
+}
+
 const router: Router = express.Router();
 const aivenApiUrl = config.aivenApiUrl;
+let dataCached: Clouds;
 
 router.get('/', async (req: Request, res: Response, next:NextFunction) => {
     try {
-        const response = await fetch(`${aivenApiUrl}/v1/clouds`);
-        const data = await response.json();
-        res.send(data);
+        if (!dataCached) {
+            const response = await fetch(`${aivenApiUrl}/v1/clouds`);
+            const data: Clouds = await response.json() as Clouds;
+            dataCached = data;
+        }
+
+        res.send(dataCached);
     } catch (error) {
         return next(error);
     }
