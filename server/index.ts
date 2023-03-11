0b5fb09d9d6cb74424ee26ae20b1386336679abf
Maxim Kholod <maxim.kholod@klarna.com>
2023-03-11 11:36:22 +0100

    init clouds route

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cloudsRoutes from './modules/clouds/routes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('Hello Aiven with Typescript!');
-});
-
+app.use('/clouds', cloudsRoutes)
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
