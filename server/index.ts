96ce6b2fadb8d4da4d9365d69097e4362af1b14c
Maxim Kholod <maxim.kholod@klarna.com>
2023-03-12 16:02:47 +0100

    allow localhost in cors

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,7 @@ const port = config.port;
 
 app.use((req: Request, res: Response, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
 
     next();
